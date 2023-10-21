8d47b210fdf6822dbe05dbaa9f3eabb0727099ba
Rakib <mohammadmohiuddin3490@gmai.com>
2023-10-22 00:08:13 +0600

    product updated

--- index.js
+++ index.js
@@ -36,17 +36,41 @@ async function run() {
       res.send(result);
     });
 
-    // app.get('product/:id', async(req, res) => {
-    //   const id = req.params.id;
-    //   const query = {_id: new ObjectId(id)}
-    //   const result = productCollection.findOne(query);
-    //   res.send(result);
-    // })
+    app.get("product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = productCollection.findOne(query);
+      res.send(result);
+    });
 
     app.post("/product", async (req, res) => {
       const newProduct = req.body;
       console.log(newProduct);
       const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
+    app.put("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateProduct = req.body;
+      const product = {
+        $set: {
+          name: updateProduct.name,
+          brand: updateProduct.brand,
+          type: updateProduct.type,
+          price: updateProduct.price,
+          description: updateProduct.description,
+          rating: updateProduct.rating,
+          photo: updateProduct.photo,
+        },
+      };
+      const result = await productCollection.updateOne(
+        filter,
+        product,
+        options
+      );
       res.send(result);
     });
 
@@ -76,12 +100,12 @@ async function run() {
       res.send(result);
     });
 
-    app.delete('/cart/:id', async(req, res) => {
+    app.delete("/cart/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
+      const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
